6ad13cd31b2898c9eabecf14d731612c567285db
Galih <galihwu2@gmail.com>
2026-04-08 13:37:31 +0700

    fix ll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomeOne from "./Components/Home/HomeOne";
 import HomeThree from "./Components/Home/HomeThree";
